363ffbdd8743846ddc554050ff2b863158e2b04e
Sayeem2004 <MohammadSSKC@gmail.com>
2026-02-20 23:10:59 -0500

    quick fix for previous search compilation, forgot to ensure that compilation works after removing previous _site

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,11 +1,10 @@
-import * as esbuild from "esbuild";
-
 export default function (eleventyConfig) {
     // Ignore .gitignore and use .eleventyignore instead
     eleventyConfig.setUseGitIgnore(false);
 
     // Copy over static resources.
     eleventyConfig.addPassthroughCopy("src/css");
+    eleventyConfig.addPassthroughCopy("src/scripts/**/*.js");
 
     // Directory configurations and template engines.
     return {
